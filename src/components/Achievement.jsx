969fe95db9d3af7d52f1354963163f0a584b91e2
Talha <talhaansari2026@gmail.com>
2025-04-11 01:27:31 +0530

    achv

--- src/components/Achievement.jsx
+++ src/components/Achievement.jsx
@@ -56,12 +56,13 @@ const Achievement = () => {
                     <p className="description">
                       Top 3 teams at SIH 2024 under Ministry of Railways, facilitated by AICTE Vice Chairman
                     </p>
-                    <a href="https://www.linkedin.com/posts/talha-ansari-22189b1bb_sih2024-smartindiahackathon-innovation-activity-7275529423381024768-VbLR?utm_source=share&utm_medium=member_desktop&rcm=ACoAADM19HkB2MGyj4JSCzp0eQM9bjQCf7j6ny4"
+                    <div className="button-wrapper"><a href="https://www.linkedin.com/posts/talha-ansari-22189b1bb_sih2024-smartindiahackathon-innovation-activity-7275529423381024768-VbLR?utm_source=share&utm_medium=member_desktop&rcm=ACoAADM19HkB2MGyj4JSCzp0eQM9bjQCf7j6ny4"
                       target="_blank"
                       className="viewmore-button"
                       rel="noopener noreferrer">
                       View More
-                    </a>
+                    </a></div>
+                    
                   </div>
                 </div>
 
@@ -76,12 +77,15 @@ const Achievement = () => {
                     <p className="description">
                       Runner-Up at Bluebit 2.0 Hackathon at PCCOE Pune , facilitated by IEEE Pune section Chair.
                     </p>
+                    <div className="button-wrapper">
                     <a href="https://www.linkedin.com/posts/talha-ansari-22189b1bb_cybertitans-hackathon-machinelearning-activity-7260309804747735040-40jK?utm_source=share&utm_medium=member_desktop&rcm=ACoAADM19HkB2MGyj4JSCzp0eQM9bjQCf7j6ny4"
                       target="_blank"
                       className="viewmore-button"
                       rel="noopener noreferrer">
                       View More
                     </a>
+                    </div>
+                    
                   </div>
                 </div>
 
@@ -96,12 +100,15 @@ const Achievement = () => {
                     <p className="description">
                       Top 12 finish at Logithon 2.0 Hackathon held at IIT Bombay orgainized by Softlink pvt.ltd
                     </p>
+                    <div className="button-wrapper">
                     <a href="https://www.linkedin.com/posts/talha-ansari-22189b1bb_hackathon-innovation-logithon-activity-7305288162472304641-uUxZ?utm_source=share&utm_medium=member_desktop&rcm=ACoAADM19HkB2MGyj4JSCzp0eQM9bjQCf7j6ny4"
                       target="_blank"
                       className="viewmore-button"
                       rel="noopener noreferrer">
                       View More
                     </a>
+                    </div>
+                    
                   </div>
                 </div>
 
@@ -116,32 +123,38 @@ const Achievement = () => {
                     <p className="description">
                       Top 5 Teams at Quasar 2.0 Hackathon at VPPCOEVA, Mumbai
                     </p>
+                    <div className="button-wrapper">
                     <a href="https://www.linkedin.com/posts/sumeet-gupta-ba2216209_quasar-smarteducation-innovation-ugcPost-7176274567353548800-N7Eb?utm_source=share&utm_medium=member_desktop&rcm=ACoAADM19HkB2MGyj4JSCzp0eQM9bjQCf7j6ny4"
                       target="_blank"
                       className="viewmore-button"
                       rel="noopener noreferrer">
                       View More
                     </a>
+                    </div>
+                    
                   </div>
                 </div>
 
                 <div className="card swiper-slide">
                   <div className="image-content">
                     <div className="card-image">
-                      <img src="images/nt.jpg" alt="" className="card-img" />
+                      <img src="images/nu.jpg" alt="" className="card-img" />
                     </div>
                   </div>
                   <div className="card-content">
                     <h2 className="name">DebugOn X IIT Delhi</h2>
                     <p className="description">
-                      Finalist DebugOn X Edc IIT Delhi with ONDC under Ministry of Industry and Internal Trade.
+                      Finalist at DebugOn X Edc IIT Delhi with ONDC under Ministry of Industry and Internal Trade.
                     </p>
+                    <div className="button-wrapper">
                     <a href="https://www.linkedin.com/posts/talha-ansari-22189b1bb_ondc-ecommerce-innovation-activity-7294771086150283268-3Ku9?utm_source=share&utm_medium=member_desktop&rcm=ACoAADM19HkB2MGyj4JSCzp0eQM9bjQCf7j6ny4"
                       target="_blank"
                       className="viewmore-button"
                       rel="noopener noreferrer">
                       View More
                     </a>
+                    </div>
+                  
                   </div>
                 </div>
                 {/* Cards end */}
