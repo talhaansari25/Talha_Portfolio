3bae12f43ab08d6c4c1f8f406cb89321c7fa2581
Talha <talhaansari2026@gmail.com>
2025-04-11 00:55:23 +0530

    skills

---
+++ src/components/Skills.jsx
@@ -0,0 +1,152 @@
+import React from "react";
+import "../style.css";
+
+
+const Skills = () => {
+    return (
+      <section id="skills" data-aos="fade-up">
+        <div className="skills-container">
+          <h3 className="skills-header">MY SKILLS</h3>
+          <div className="skills-wrapper" data-aos="fade-up">
+  
+            <div className="skill">
+              <div className="skills-logo">
+                <img src="./icons/html-5.webp" alt="" />
+              </div>
+              <span>HTML</span>
+            </div>
+  
+            <div className="skill">
+              <div className="skills-logo">
+                <img src="./icons/css-3.webp" alt="" />
+              </div>
+              <span>CSS</span>
+            </div>
+  
+            <div className="skill">
+              <div className="skills-logo">
+                <img src="./icons/js.webp" alt="" />
+              </div>
+              <span>JavaScript</span>
+            </div>
+  
+            <div className="skill">
+              <div className="skills-logo">
+                <img src="./icons/nodejs.webp" alt="" />
+              </div>
+              <span>NodeJS</span>
+            </div>
+  
+            <div className="skill">
+              <div className="skills-logo">
+                <img src="./icons/express.webp" alt="" />
+              </div>
+              <span>ExpressJS</span>
+            </div>
+  
+            <div className="skill">
+              <div className="skills-logo">
+                <img src="./icons/react.webp" alt="" />
+              </div>
+              <span>React.JS</span>
+            </div>
+  
+            <div className="skill">
+              <div className="skills-logo">
+                <img src="./icons/mongo.webp" alt="" />
+              </div>
+              <span>MongoDB</span>
+            </div>
+  
+            <div className="skill">
+              <div className="skills-logo">
+                <img src="./icons/c.webp" alt="" />
+              </div>
+              <span>C</span>
+            </div>
+  
+            <div className="skill">
+              <div className="skills-logo">
+                <img src="./icons/cpp.webp" alt="" />
+              </div>
+              <span>C++</span>
+            </div>
+  
+            <div className="skill">
+              <div className="skills-logo">
+                <img src="./icons/python.webp" alt="" />
+              </div>
+              <span>Python</span>
+            </div>
+  
+            <div className="skill">
+              <div className="skills-logo">
+                <img src="./icons/java.webp" alt="" />
+              </div>
+              <span>Java</span>
+            </div>
+  
+            <div className="skill">
+              <div className="skills-logo">
+                <img src="./icons/git.webp" alt="" />
+              </div>
+              <span>Git</span>
+            </div>
+  
+            <div className="skill">
+              <div className="skills-logo">
+                <img src="./icons/github.webp" alt="" />
+              </div>
+              <span>GitHub</span>
+            </div>
+  
+            <div className="skill">
+              <div className="skills-logo">
+                <img src="./icons/mysql.webp" alt="" />
+              </div>
+              <span>MySQL</span>
+            </div>
+  
+            <div className="skill">
+              <div className="skills-logo">
+                <img src="./icons/bootstrap.webp" alt="" />
+              </div>
+              <span>Bootstrap</span>
+            </div>
+  
+            <div className="skill">
+              <div className="skills-logo">
+                <img src="./icons/tailwind.webp" alt="" />
+              </div>
+              <span>Tailwind</span>
+            </div>
+  
+            <div className="skill">
+              <div className="skills-logo">
+                <img src="./icons/figma.webp" alt="" />
+              </div>
+              <span>Figma</span>
+            </div>
+  
+            <div className="skill">
+              <div className="skills-logo">
+                <img src="./icons/can.webp" alt="" />
+              </div>
+              <span>Canva</span>
+            </div>
+  
+            <div className="skill">
+              <div className="skills-logo">
+                <img src="./icons/premiere.webp" alt="" />
+              </div>
+              <span>Premier Pro</span>
+            </div>
+  
+          </div>
+        </div>
+      </section>
+    );
+  };
+  
+  export default Skills;
+  
