40dc2105be9350a82b1115f33ed0051c103ef28d
Talha <talhaansari2026@gmail.com>
2025-04-24 23:53:53 +0530

    project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -73,19 +73,27 @@ const Projects = () => {
             {
               title: "Rail Sahayta",
               images: ["ocr.png", "add comp.jpeg", "category , subcategory detection.jpeg"],
-              description: `Rail Sahayta is an AI-Powered Grievance Management System for Indian Railways...`,
+              description: `Rail Sahayta is an AI-Powered Grievance Management System for Indian Railways which is an enhanced version of Rail Madad using AI for smarter complaint handling.
+It categorizes complains from text, images, audio & video using models like VGG16, YOLOv5, Mask RCNN, U-Net & PaddleOCR. Severity is auto-detected and routed for faster resolution.
+Features include Hindi voice support, SOS alerts, predictive ARIMA-based issue tracking, and a chatbot powered by Gemma 2B and lot many more..
+Check out my work on GitHub!`,
               github: "https://www.github.com/talhaansari25/RailSahayta_SIH24"
             },
             {
               title: "NewsSnips",
               images: ["main.png", "sum.png", "list.png"],
-              description: `NewsSnips is a Global News Categorization & Summarization platform...`,
+              description: `NewsSnips is a Global News Categorization & Summarization platform , A smart news application that categorizes and summarizes global news using NLP. Built with React.js for smooth UI, it uses TF-IDF & Cosine Similarity for summarization.
+Features include real-time updates, custom keyword search, and interactive filtering for personalized news delivery.
+Check out my work on GitHub!`,
               github: "https://www.github.com/talhaansari25/NewsSnips"
             },
             {
               title: "Global Catalog Registry - GCR",
               images: ["sp.png", "sd.png", "su.png", "mu.png", "bp.png"],
-              description: `A Centralized Database that efficiently manages and organizes product catalogs...`,
+              description: `A Centrailzed DataBase that Efficiently manages and organizes product catalogs from multiple sellers for seamless access and retrieval.
+Global Catalog Registry (GCR) aims to streamline catalog management within the ONDC ecosystem.
+It supports pre-built and custom templates, real-time dynamic data handling, and unique product identification, ensuring smooth seller onboarding and optimized e-commerce operations.
+Check out my work on GitHub!`,
               github: "https://www.github.com/talhaansari25/GCR-Global_Catalog_Registry"
             },
             {
@@ -97,7 +105,9 @@ const Projects = () => {
                 "Single Shipment Upload.jpeg",
                 "Multiple Shipments Upload (CSV).jpeg"
               ],
-              description: `LogiComply is an AI-Powered Compliance Checker for Cross-Border Shipments...`,
+              description: `LogiComply is an AI-Powered Compliance Checker for Cross-Border Shipments , An intelligent system that validates international shipments using rule-based logic and ML models like XGBoost to detect anomalies and flag high-risk consignments.
+Features include real-time dashboards, compliance analytics, and separate admin/user portals. Built with React.js (frontend), Node.js/Express.js (backend), MongoDB, and Flask for ML integration—ensuring secure, regulation-compliant logistics.
+Check out my work on GitHub!`,
               github: "https://www.github.com/talhaansari25/Logi-Comply"
             },
             {
@@ -109,7 +119,9 @@ const Projects = () => {
                 "Assesment Generation.jpeg",
                 "summary.jpeg"
               ],
-              description: `This AI-driven learning platform enhances student education by integrating automated assessments...`,
+              description: `This AI-driven learning platform enhances student education by integrating automated assessments, AI-generated summaries, gamified coding challenges, and intelligent course recommendations.
+Using Transformer T5 for lecture summarization and LLaVA for Q&A and assessments, the platform provides a personalized learning experience with instant feedback and interactive challenges.
+Check out my work on GitHub!`,
               github: "https://www.github.com/talhaansari25/IRIS-AI_Powered_Educational_Platform-"
             }
           ].map((project, idx) => (
@@ -127,7 +139,8 @@ const Projects = () => {
               </div>
               <div className={idx % 2 === 0 ? "one-text" : "two-text"}>
                 <h3 className="title">{project.title}</h3>
-                <p className="detail">{project.description}</p>
+                <p className="detail" style={{ whiteSpace: 'pre-line' }}>{project.description}</p>
+
                 <a href={project.github}>LINK &gt;</a>
               </div>
             </div>
