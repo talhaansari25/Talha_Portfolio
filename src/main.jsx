dc4a2ef20e37df954f3588cd8f8a314a3a06e3dd
Talha <talhaansari2026@gmail.com>
2025-04-25 18:53:44 +0530

    main

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import './style.css'; // make sure this is here if it's required
+import './style.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
