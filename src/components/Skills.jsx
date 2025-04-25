603210e718c0e4f58715017da6c12cc66f13d5e6
Talha <talhaansari2026@gmail.com>
2025-04-25 22:17:14 +0530

    skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,33 +1,50 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "../style.css";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
+// Import all skill icons
+import htmlIcon from "/icons/html-5.webp";
+import cssIcon from "/icons/css-3.webp";
+import jsIcon from "/icons/js.webp";
+import nodeIcon from "/icons/nodejs.webp";
+import expressIcon from "/icons/express.webp";
+import reactIcon from "/icons/react.webp";
+import mongoIcon from "/icons/mongo.webp";
+import cIcon from "/icons/c.webp";
+import cppIcon from "/icons/cpp.webp";
+import pythonIcon from "/icons/python.webp";
+import javaIcon from "/icons/java.webp";
+import gitIcon from "/icons/git.webp";
+import githubIcon from "/icons/github.webp";
+import mysqlIcon from "/icons/mysql.webp";
+import bootstrapIcon from "/icons/bootstrap.webp";
+import tailwindIcon from "/icons/tailwind.webp";
+import figmaIcon from "/icons/figma.webp";
+import canvaIcon from "/icons/can.webp";
+
 const Skills = () => {
-  // Skills data in JSON format
   const skillsData = [
-    { name: "HTML", icon: "./icons/html-5.webp" },
-    { name: "CSS", icon: "./icons/css-3.webp" },
-    { name: "JavaScript", icon: "./icons/js.webp" },
-    { name: "NodeJS", icon: "./icons/nodejs.webp" },
-    { name: "ExpressJS", icon: "./icons/express.webp" },
-    { name: "React.JS", icon: "./icons/react.webp" },
-    { name: "MongoDB", icon: "./icons/mongo.webp" },
-    { name: "C", icon: "./icons/c.webp" },
-    { name: "C++", icon: "./icons/cpp.webp" },
-    { name: "Python", icon: "./icons/python.webp" },
-    { name: "Java", icon: "./icons/java.webp" },
-    { name: "Git", icon: "./icons/git.webp" },
-    { name: "GitHub", icon: "./icons/github.webp" },
-    { name: "MySQL", icon: "./icons/mysql.webp" },
-    { name: "Bootstrap", icon: "./icons/bootstrap.webp" },
-    { name: "Tailwind", icon: "./icons/tailwind.webp" },
-    { name: "Figma", icon: "./icons/figma.webp" },
-    { name: "Canva", icon: "./icons/can.webp" },
-    
+    { name: "HTML", icon: htmlIcon },
+    { name: "CSS", icon: cssIcon },
+    { name: "JavaScript", icon: jsIcon },
+    { name: "NodeJS", icon: nodeIcon },
+    { name: "ExpressJS", icon: expressIcon },
+    { name: "React.JS", icon: reactIcon },
+    { name: "MongoDB", icon: mongoIcon },
+    { name: "C", icon: cIcon },
+    { name: "C++", icon: cppIcon },
+    { name: "Python", icon: pythonIcon },
+    { name: "Java", icon: javaIcon },
+    { name: "Git", icon: gitIcon },
+    { name: "GitHub", icon: githubIcon },
+    { name: "MySQL", icon: mysqlIcon },
+    { name: "Bootstrap", icon: bootstrapIcon },
+    { name: "Tailwind", icon: tailwindIcon },
+    { name: "Figma", icon: figmaIcon },
+    { name: "Canva", icon: canvaIcon },
   ];
 
- 
   useEffect(() => {
     AOS.init({
       duration: 800,
@@ -42,16 +59,16 @@ const Skills = () => {
         <h3 className="skills-header">MY SKILLS</h3>
         <div className="skills-wrapper" data-aos="fade-up">
           {skillsData.map((skill, index) => (
-            <div 
+            <div
               key={skill.name}
               className="skill"
               data-aos="fade-up"
               data-aos-delay={index * 50}
             >
               <div className="skills-logo">
-                <img 
-                  src={skill.icon} 
-                  alt={skill.name} 
+                <img
+                  src={skill.icon}
+                  alt={skill.name}
                   loading="lazy"
                 />
               </div>
