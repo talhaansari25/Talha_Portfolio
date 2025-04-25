5323b7bc2576477b6559941f632d4629a8c6563d
Talha <talhaansari2026@gmail.com>
2025-04-25 18:54:20 +0530

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,9 @@ import "../style.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
-
 import About from "./About";
 import Portfolio from "./Portfolio";
 import Achievement from "./Achievement";
-import Projects from "../pages/Projects";
-import Handles from "../pages/Handles";
 import Skills from "./Skills";
 import Footer from "./Footer";
 import Main from "./Main";
