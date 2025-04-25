1c6ce26682302a73ac0af01a6bc80a1fabdf0cb9
Talha <talhaansari2026@gmail.com>
2025-04-25 18:45:36 +0530

    main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import { Link } from "react-router-dom";
 import "../style.css";
+
+
 
 const Main = () => {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
